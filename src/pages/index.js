67d44f5aef54c1948e5d0de76e309f668ecf58fd
Olena Makovey <ellen.makovey@gmail.com>
2019-01-16 08:59:00 -0800

    Added div class to index.js, cleaned up the css a bit.

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,49 +42,57 @@ export default class IndexPage extends React.Component {
             fieldset(disabled='') - to disable the fields */}
             <fieldset>
               <legend>Create Schedule</legend>
-              <label for="schedule">Payments</label>
-              {/* How often are the payments: weekly, bi-weekly, monthly. For weekly/bi-weekly choice we'll have another option showing. */}
-              <input
-                list="schedules"
-                id="schedule"
-                name="schedule"
-                value={this.state.payments}
-                onChange={this.handleInputChange}
-              />
-              <datalist id="schedules">
-                <option value="Weekly" />
-                <option value="Bi-Weekly" />
-                <option value="Monthly" />
-              </datalist>
+              <div class=".form-field">
+                <label for="schedule">Payments</label>
+                {/* How often are the payments: weekly, bi-weekly, monthly. For weekly/bi-weekly choice we'll have another option showing. */}
+                <input
+                  list="schedules"
+                  id="schedule"
+                  name="schedule"
+                  value={this.state.payments}
+                  onChange={this.handleInputChange}
+                />
+                <datalist id="schedules">
+                  <option value="Weekly" />
+                  <option value="Bi-Weekly" />
+                  <option value="Monthly" />
+                </datalist>
+              </div>
               {/* Calendar that allows to choose the date of 'Payments Start' */}
-              <label for="payStart">Start Date</label>
-              <input
-                type="date"
-                id="payStart"
-                name="payStart"
-                value={this.state.payStart}
-                onChange={this.handleInputChange}
-              />
-              <label for="years">Mortgage Period (years)</label>
-              <input
-                type="number"
-                id="years"
-                name="years"
-                min='1'
-                max='99'
-                value={this.state.years}
-                onChange={this.handleInputChange}
-              />
-              <label for="step">Periodicity (months)</label>
-              <input
-                type="number"
-                id="step"
-                name="step"
-                min='1'
-                max='12'
-                value={this.state.step}
-                onChange={this.handleInputChange}
-              />
+              <div class=".form-field">
+                <label for="payStart">Start Date</label>
+                <input
+                  type="date"
+                  id="payStart"
+                  name="payStart"
+                  value={this.state.payStart}
+                  onChange={this.handleInputChange}
+                />
+              </div>
+              <div class=".form-field">
+                <label for="years">Mortgage Period (years)</label>
+                <input
+                  type="number"
+                  id="years"
+                  name="years"
+                  min='1'
+                  max='99'
+                  value={this.state.years}
+                  onChange={this.handleInputChange}
+                />
+              </div>
+              <div class=".form-field">
+                <label for="step">Periodicity (months)</label>
+                <input
+                  type="number"
+                  id="step"
+                  name="step"
+                  min='1'
+                  max='12'
+                  value={this.state.step}
+                  onChange={this.handleInputChange}
+                />
+              </div>
             </fieldset>
             <button type="submit">Next</button>
           </form>
@@ -95,36 +103,42 @@ export default class IndexPage extends React.Component {
             fieldset(disabled='') - to disable the fields */}
             <fieldset>
               <legend>Mortgage Set-Up</legend>
-              <label for="housePrice">House Price</label>
-              {/* How often are the payments: weekly, bi-weekly, monthly. For weekly/bi-weekly choice we'll have another option showing. */}
-              <input
-                type="date"
-                id="housePrice"
-                name="housePrice"
-                min='1'
-                value={this.state.housePrice}
-                onChange={this.handleInputChange}
-              />
-              <label for="interest">Interest Rate (%)</label>
-              <input
-                type="number"
-                id="interest"
-                name="interest"
-                min='0'
-                max='6'
-                step='0.01'
-                value={this.state.interest}
-                onChange={this.handleInputChange}
-              />
-              <label for="downpayment">Downpayment</label>
-              <input
-                type="number"
-                id="downpayment"
-                name="downpayment"
-                min='0'
-                value={this.state.downpayment}
-                onChange={this.handleInputChange}
-              />
+              <div class=".form-field">
+                <label for="housePrice">House Price</label>
+                {/* How often are the payments: weekly, bi-weekly, monthly. For weekly/bi-weekly choice we'll have another option showing. */}
+                <input
+                  type="date"
+                  id="housePrice"
+                  name="housePrice"
+                  min='1'
+                  value={this.state.housePrice}
+                  onChange={this.handleInputChange}
+                />
+              </div>
+              <div class=".form-field">
+                <label for="interest">Interest Rate (%)</label>
+                <input
+                  type="number"
+                  id="interest"
+                  name="interest"
+                  min='0'
+                  max='6'
+                  step='0.01'
+                  value={this.state.interest}
+                  onChange={this.handleInputChange}
+                />
+              </div>
+              <div class=".form-field">
+                <label for="downpayment">Downpayment</label>
+                <input
+                  type="number"
+                  id="downpayment"
+                  name="downpayment"
+                  min='0'
+                  value={this.state.downpayment}
+                  onChange={this.handleInputChange}
+                />
+              </div>
             </fieldset>
             {/* Show payments schedule on the screen. */}
             <button type="submit">Submit</button>
